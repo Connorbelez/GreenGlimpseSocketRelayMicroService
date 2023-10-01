5725c08be5d66b75f556ce6df4b4e01199639d89
Connor Beleznay <67024888+Connorbelez@users.noreply.github.com>
2023-10-01 19:13:45 -0400

    Update samplePublisher.js

--- samplePublisher.js
+++ samplePublisher.js
@@ -1,32 +1,8 @@
-const url = 'wss://mr-connection-y5r00qk8jwd.messaging.solace.cloud:443';
-const vpn = 'greenglimpseevents';
-const username = 'solace-cloud-client';
-const pass = '';
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+//=============SOLACE CREDENTIALS GO HERE===============
 
-/**
- * Solace Systems Node.js API
- * Publish/Subscribe tutorial - Topic Publisher
- * Demonstrates publishing direct messages to a topic
- */
+
+//======================================================
 
 /*jslint es6 node:true devel:true*/
 const readline = require('readline');
